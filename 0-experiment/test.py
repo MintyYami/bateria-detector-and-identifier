013530cd38273df1105dcc040f82d94e3ed7e6e5
MintyYami <minthappyworld@gmail.com>
2026-04-29 15:35:38 +0100

    add data-7

--- 0-experiment/test.py
+++ 0-experiment/test.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-dir_path_data = "C:/Users/HP/Desktop/Warwick/Hackathon/data/SYTO-9_WGA647_BF_ECOLI_STAPH_1_10000/pos_0"
-dir_path = "C:/Users/HP/Desktop/Warwick/Hackathon/BioImage/data/pos_84"
+dir_path_data = "C:/Users/HP/Desktop/Warwick/Hackathon/data/SYTO-9_WGA_STAPH_PLL/pos_0"
+dir_path = "C:/Users/HP/Desktop/Warwick/Hackathon/BioImage/data/pos_102"
 for file in os.listdir(dir_path_data):
     if file.endswith(".tif"):
         print(file)
