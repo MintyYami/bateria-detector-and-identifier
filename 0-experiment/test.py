fe803526c745d1faed8a2482f8118d38b1974325
MintyYami <minthappyworld@gmail.com>
2026-04-29 15:20:22 +0100

    add data-6

--- 0-experiment/test.py
+++ 0-experiment/test.py
@@ -1,2 +1,9 @@
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
+import shutil
+
+dir_path_data = "C:/Users/HP/Desktop/Warwick/Hackathon/data/SYTO-9_WGA647_BF_ECOLI_STAPH_1_10000/pos_0"
+dir_path = "C:/Users/HP/Desktop/Warwick/Hackathon/BioImage/data/pos_84"
+for file in os.listdir(dir_path_data):
+    if file.endswith(".tif"):
+        print(file)
+        shutil.copyfile(dir_path_data, dir_path)
